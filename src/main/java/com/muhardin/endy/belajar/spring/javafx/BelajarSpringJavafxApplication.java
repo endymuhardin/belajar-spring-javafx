fd6c765ab53ab5ca6efc5aba60c17a5ddf41c3d8
Endy Muhardin <endy.muhardin@gmail.com>
2021-10-21 10:49:42 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/spring/javafx/BelajarSpringJavafxApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.spring.javafx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringJavafxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringJavafxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhardin/endy/belajar/spring/javafx/BelajarSpringJavafxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.spring.javafx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringJavafxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
