358223321e2b642ae66a2a9d6a1d12857a28dcb4
Endy Muhardin <endy.muhardin@gmail.com>
2021-10-26 15:29:16 +0700

    integrasi dengan FXML dan javafx-weaver

--- src/main/java/com/muhardin/endy/belajar/spring/javafx/PrimaryStageInitializer.java
+++ src/main/java/com/muhardin/endy/belajar/spring/javafx/PrimaryStageInitializer.java
@@ -1,17 +1,32 @@
 package com.muhardin.endy.belajar.spring.javafx;
 
+import com.muhardin.endy.belajar.spring.javafx.controller.MainController;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import net.rgielen.fxweaver.core.FxWeaver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component @Slf4j
 public class PrimaryStageInitializer implements ApplicationListener<StageReadyEvent> {
 
+    private final FxWeaver fxWeaver;
+
+    @Autowired
+    public PrimaryStageInitializer(FxWeaver fxWeaver) {
+        this.fxWeaver = fxWeaver;
+    }
+
     @Override
     public void onApplicationEvent(StageReadyEvent event) {
         log.debug("Running StageReadyEvent handler");
-        Stage eventSource = (Stage) event.getSource();
-        eventSource.show();
+        Stage stage = event.stage;
+        Parent root = fxWeaver.loadView(MainController.class);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/com/muhardin/endy/belajar/spring/javafx/controller/MainController.java
@@ -0,0 +1,9 @@
+package com.muhardin.endy.belajar.spring.javafx.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component @Slf4j @FxmlView
+public class MainController {
+}
